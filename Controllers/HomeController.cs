b154d21908d5eca512f2e7a4a29d5823780c3fd4
justinJSwebdev <trankyhung225@gmail.com>
2024-01-28 14:23:09 +0700

    layout, partial, component

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,13 @@ namespace razor_asp.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/about")]
+        public IActionResult About()
         {
             return View();
         }
