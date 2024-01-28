876800242ea468f015fa530d7be421fe8f2488e1
justinJSwebdev <trankyhung225@gmail.com>
2024-01-28 21:48:53 +0700

    update

---
+++ Controllers/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace razor_asp.Controllers.ViewComponents
+{
+    public class GridViewComponent :  ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
